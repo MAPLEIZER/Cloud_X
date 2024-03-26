82628a1bc2c929f7345f607032de7adc715b2972
MAPLEIZER <30689990+MAPLEIZER@users.noreply.github.com>
2024-03-26 10:54:49 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+from setuptools import setup
+
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name                =   "cloudx",
+    version             =   '1.0',
+    description         =   "A Multi-Tool Web Vulnerability Scanner.",
+    url                 =   "https://github.com/skavngr/rapidscan",
+    author              =   "sh4nx0r",
+    py_modules          =   ['rapidscan',],
+    install_requires    =   [],
+    python_requires=">=3.6",
+)
